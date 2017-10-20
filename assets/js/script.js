39816318981e5b6e2120067cf6737fa72df2079b
Darryl <darryl.dolan@hotmail.co.uk>
2017-10-20 15:40:07 +0200

    PureCSS added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
   var active5 = false;
 
   $('.main-nav').on('mousedown touchstart', function() {
-  // Magic code
+  // Magic code 
   // $().toggleClass();
   // Check current menu state, then changes CSS.
   console.log('Changing menu state...');
   if (!active1) {
-    $(this).find('.test1').css({'transform': 'translate(100px,0px)'});
+    $(this).find('.­­test1').css({'transform': 'translate(100px,0px)'});
     // Make link text appear
     $(this).find('.nav-link').css({'opacity': '0.5'});
   } else {
@@ -48,5 +48,25 @@ $(document).ready(function() {
   active4 = !active4;
   active5 = !active5;
   });
-
+  // Slider
+  $(function() {
+    $(".rslides").responsiveSlides({
+      auto: true,             // Boolean: Animate automatically, true or false
+      speed: 500,             // Integer: Speed of the transition, in milliseconds
+      timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+      pager: false,           // Boolean: Show pager, true or false
+      nav: false,             // Boolean: Show navigation, true or false
+      random: false,          // Boolean: Randomize the order of the slides, true or false
+      pause: false,           // Boolean: Pause on hover, true or false
+      pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+      prevText: "Previous",   // String: Text for the "previous" button
+      nextText: "Next",       // String: Text for the "next" button
+      maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+      navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+      manualControls: "",     // Selector: Declare custom pager navigation
+      namespace: "rslides",   // String: Change the default namespace used
+      before: function(){},   // Function: Before callback
+      after: function(){}     // Function: After callback
+    });
+  });
 });
