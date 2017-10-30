4e445867188d67d3fab2a68ce7974a2adc346f72
Darryl <darryl.dolan@hotmail.co.uk>
2017-10-30 11:31:09 +0100

    Added outlines and borders to heading and photo elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
       timeout: 10000,          // Integer: Time between slide transitions, in milliseconds
       pager: false,           // Boolean: Show pager, true or false
       nav: false,             // Boolean: Show navigation, true or false
-      random: false,          // Boolean: Randomize the order of the slides, true or false
+      random: true,          // Boolean: Randomize the order of the slides, true or false
       pause: false,           // Boolean: Pause on hover, true or false
       pauseControls: true,    // Boolean: Pause when hovering controls, true or false
       prevText: "Previous",   // String: Text for the "previous" button
